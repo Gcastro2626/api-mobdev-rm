9c49fcd757e45602a301ecf47bcbad12e8ace868
Gerson Castro <gerson.castro@cocha.com>
2022-03-25 22:56:07 -0300

    initial commit

---
+++ src/test/java/com/mobdev/apimobdevrm/ApiMobdevRmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobdev.apimobdevrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMobdevRmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
